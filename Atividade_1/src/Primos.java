a62e822d3500b5c3bc541d96ce5e13a43a67c565
leobtsd <leonardobts8@gmail.com>
2024-03-03 12:10:18 -0300

    Correção do script primos

--- Atividade_1/src/Primos.java
+++ Atividade_1/src/Primos.java
@@ -1,42 +1,33 @@
 import java.util.Random;
-import java.util.Scanner;
 
 public class Primos {
 
     public static void main(String[] args) {
-        
-    
+        int[] numeros = new int[100];
+        Random random = new Random();
 
-    Scanner scanner = new Scanner(System.in);
-
-    Random random = new Random();
-
-    int[] array = new int[100];
-
-    for(int i = 0; i < 100; i++) {
-
-        array[i] = random.nextInt(200);
-        
-    }
-    for (int i = 0; i < 100; i++) {
-        if (array[i] <= 1) {
-            System.out.println("O número 1 não é composto e nem considerado um número primo");
-        } else {
-            
+        for (int i = 0; i < 100; i++) {
+            numeros[i] = random.nextInt(200); 
         }
 
-        System.out.println(array[i]);
+        System.out.println("Números primos no array:");
 
+        for (int i = 0; i < 100; i++) {
+            if (ehPrimo(numeros[i])) {
+                System.out.print(numeros[i] + " ");
+            }
+        }
     }
-    System.out.println("Inverter..");
 
-    for (int i = 99; i >= 0; i--) {
-
-        System.out.println(array[i]);
-
+    public static boolean ehPrimo(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
-    
-    scanner.close();
-    }
-    
 }
